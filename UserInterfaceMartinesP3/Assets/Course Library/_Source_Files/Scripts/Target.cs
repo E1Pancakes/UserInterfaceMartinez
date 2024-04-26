5bdabd82c5772c2d82e8d99682cf5df29147d764
Diego Martinez <718717@student.dusd.net>
2024-04-26 11:40:36 -0700

    I finished 5.2
    i just need the game over

--- UserInterfaceMartinesP3/Assets/Course Library/_Source_Files/Scripts/Target.cs
+++ UserInterfaceMartinesP3/Assets/Course Library/_Source_Files/Scripts/Target.cs
@@ -6,17 +6,21 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float yRangePos = -6;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
-        targetRb.AddTorque(RandomTorque(), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos(); 
     }
 
@@ -24,6 +28,18 @@ public class Target : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation); 
+        gameManager.UpdateScore(pointValue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
     Vector3 RandomForce()
@@ -38,6 +54,6 @@ public class Target : MonoBehaviour
 
     Vector3 RandomSpawnPos()
     {
-        return new Vector3(Random.Range(-xRange, xRange), -yRangePos);
+        return new Vector3(Random.Range(-xRange, xRange), yRangePos);
     }
 }
